b97bcd35d02c1970b31f39586a08b311bd61ebc8
unknown <vmariaanyelin@gmal.com>
2021-07-29 12:04:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+app.get('/', (req, res) => {
+  console.log('res',res);
+  res.send('Hello World!');
+});
+// http://localhost:8000/
+app.post('/create', (req, res) => {
+  res.send('Create Reminder');
+});
+app.listen(port, () => {
+  console.log(`====Example app listening at http://localhost:${port}=====`);
+});
